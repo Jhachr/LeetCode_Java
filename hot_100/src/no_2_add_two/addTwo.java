7b94cc4cc3e644d8e0340940c1a80f24b153f1fb
jhachr <jhachr@qq.com>
2022-07-07 23:36:46 +0800

    记录刷题思路

--- hot_100/src/no_2_add_two/addTwo.java
+++ hot_100/src/no_2_add_two/addTwo.java
@@ -6,6 +6,13 @@ import java.util.Objects;
 
 public class addTwo {
 
+    /*
+        这里主要使用了递归的办法，发现这里其实分三种情况，
+        1.当两个节点都还在，则按照进位+
+        2.当两个节点都为NUll，如果进位为0，则直接返回，如果进位为1，则新建一个为1的node
+        3.当其中一个节点为null时，其实处理的逻辑是 用不为空的节点跟一个值为flag的node进行第一种处理，这里其实是一种递归。
+        完成！
+     */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode startNode = new ListNode(-1);
         ListNode now = startNode;
